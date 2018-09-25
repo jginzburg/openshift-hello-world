ab23a06bb5dfc55b0d52bbf7f6f3ef42604aa9d2
Leandro Beretta <lea.beretta@gmail.com>
2018-09-25 20:46:11 -0300

    mensaje para develop

--- src/main/java/io/veicot/openshift/rest/HelloService.java
+++ src/main/java/io/veicot/openshift/rest/HelloService.java
@@ -14,6 +14,6 @@ public class HelloService extends Application {
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from OpenShift!").build();
+		return Response.ok("Hello from OpenShift! (develop)").build();
 	}
 }

f690ae1877994cc5aa954d91a1c3c56269eb2c3c
Leandro Beretta <lea.beretta@gmail.com>
2018-09-30 15:36:36 -0300

    release 1.0.0

--- src/main/java/io/veicot/openshift/rest/HelloService.java
+++ src/main/java/io/veicot/openshift/rest/HelloService.java
@@ -14,6 +14,6 @@ public class HelloService extends Application {
 	@GET
 	@Produces("text/plain")
 	public Response doGet() {
-		return Response.ok("Hello from OpenShift! (develop)").build();
+		return Response.ok("Hello from OpenShift! (release/1.0.0)").build();
 	}
 }
